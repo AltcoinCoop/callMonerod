e1991871b98b7e4d45333ffd39963e38ebda363c
Jakub Trnka <jakub.trnka@chainanalytics.net>
2017-11-03 02:16:14 +0100

    non JSON-RPC calls

--- callMonerod.py
+++ callMonerod.py
@@ -3,15 +3,14 @@
 import requests
 import json
 
-monerod = "http://localhost:18081/json_rpc"
 hdr = {'content-type':'application/json'}
+monerod = "http://localhost:18081/"
 
-#x = json.dumps(hdr)
 
-def callMonerod( f ):
+def call_jsonrpc( f ):
     def req( *args ):
         jsonCall = f( *args )
-        r = requests.post( monerod, data=f( *args ), headers = hdr )
+        r = requests.post( monerod + "json_rpc", data=f( *args ), headers = hdr )
         #return json.dumps( r.json(), indent=4 )
         try:
             result = r.json()
@@ -26,7 +25,26 @@ def callMonerod( f ):
     return req
         
 
-def rpcMessage( f ):
+def wrap_and_call_rpc( f ):
+    def message( *args ):
+        command = f( *args )
+        resource = command[0]
+        if len( command[1] ) > 0:
+            r = requests.post( monerod + resource, data=json.dumps( command[1] ), headers = hdr )
+        else:
+            r = requests.post( monerod + resource, headers = hdr )
+        if r.status_code != 200:
+            return "Some error occured"
+        try:
+            result = r.json()
+        except json.decoder.JSONDecodeError:
+            return r.text
+
+
+        return json.dumps( result, indent = 4 )
+    return message
+
+def wrap_to_json_rpc( f ):
     def message( *args ):
         command = f( *args )
         payload = { "id": "0", "jsonrpc": "2.0", "method": command[0] }
@@ -36,43 +54,43 @@ def rpcMessage( f ):
     return message
 
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getblockcount():
     return [ "getblockcount", dict() ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def on_getblockhash( height ):
     return [ "on_getblockhash", [ height ] ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getblocktemplate( address, reserve_size ):
     return [ "getblocktemplate", {"wallet_address": address, "reserve_size": reserve_size} ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def submitblock( blockHex ):
     return [ "submitblock", [blockHex] ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getlastblockheader():
     return [ "getlastblockheader", dict() ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getblockheaderbyhash( height ):
     return [ "getblockheaderbyhash", {"hash": height} ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getblockheaderbyheight( height ):
     return [ "getblockheaderbyheight", {"height": height} ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def getblock( heightOrHash ):
     if type( heightOrHash ) == int:
         paramname = "height"
@@ -80,31 +98,60 @@ def getblock( heightOrHash ):
         paramname = "hash"
     return [ "getblock", { paramname: heightOrHash } ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def get_connections():
     return [ "get_connections", dict() ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def get_info():
     return [ "get_info", dict() ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def hard_fork_info():
     return [ "hard_fork_info", dict() ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def set_bans( banlist ):
     return [ "set_bans", { "bans":[{"ip":i[0], "ban": i[1], "seconds": i[2]} for i in banlist]} ]
 
-@callMonerod
-@rpcMessage
+@call_jsonrpc
+@wrap_to_json_rpc
 def get_bans():
     return [ "get_bans", dict() ]
 
+########################################################################
+###          other RPC methods
+
+@wrap_and_call_rpc
+def getheight():
+    return ["getheight", dict() ]
+
+@wrap_and_call_rpc
+def gettransactions( hashlist, decode_as_json = True ):
+    return ["gettransactions", {"txs_hashes": hashlist, "decode_as_json": decode_as_json} ]
+
+@wrap_and_call_rpc
+def is_key_image_spent( key_img_list ):
+    return ["is_key_image_spent", { "key_imgages": key_img_list } ]
+
+@wrap_and_call_rpc
+def sendrawtransaction( hash ):
+    return ["sendrawtransaction", {"tx_as_hex": hash} ]
+
+@wrap_and_call_rpc
+def get_transaction_pool():
+    return ["get_transaction_pool", dict() ]
+
+@wrap_and_call_rpc
+def stop_daemon():
+    return ["stop_daemon", dict() ]
+
+
+#json-rpc calls
 
 #print( getblockcount() )
 #print( on_getblockhash( 1000000 ) )
@@ -119,3 +166,13 @@ def get_bans():
 #print( hard_fork_info() )
 #print( set_bans( [(838969536, True, 30), (838969535, True, 30) ] ) )
 #print( get_bans() )
+
+
+#### other rpc calls
+
+#print( getheight() )
+#print( gettransactions( ["11c7871239322547156db5ffe556edbde4b915fc6ca701a0fac9a3256a3c3733", "9670f03031541be633c3781d00eb69ee8774e4d079c17a5fb132bf4c83c061fb"] ) )
+#print( is_key_image_spent( ["8d1bd8181bf7d857bdb281e0153d84cd55a3fcaa57c3e570f4a49f935850b5e3", "7319134bfc50668251f5b899c66b005805ee255c136f0e1cecbb0f3a912e09d4"] ) )
+#print( sendrawtransaction( "510ee3c4e14330a7b96e883c323a60ebd1b5556ac1262d0bc03c24a3b785516f" ) )
+#print( get_transaction_pool() )
+#print( stop_daemon() )
